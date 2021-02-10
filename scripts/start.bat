ec87bb85c8e0202431878c293ac05f6dcd4298f3
oureveryday <oureveryday@163.com>
2021-02-10 16:34:04 +0800

    Updated start.bat
    Automatically change aria2c.exe priority to high/realtime(With administrator rights only).

--- scripts/start.bat
+++ scripts/start.bat
@@ -4,4 +4,5 @@ for /f "tokens=2 delims==; " %%A in (
   'wmic process call create "%~dp0AriaNg_Native.exe"' ^| find "ProcessId"'
 ) do set PID=%%A
 cd /D %~dp0resources\aria2\
-start "" %~dp0resources\aria2\aria2c.exe -D true --conf-path="%~dp0resources\aria2\aria2.conf" --stop-with-process=%PID%
+start "" %~dp0resources\aria2\aria2c.exe -D true --conf-path="%~dp0resources\aria2\aria2.conf" --stop-with-process=%PID% 
+wmic process where name='aria2c.exe' call SetPriority "Realtime"
