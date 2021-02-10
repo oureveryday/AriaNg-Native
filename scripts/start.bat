1e50a5c22417020b8a3720b1fbc6c467a53522b8
oureveryday <oureveryday@163.com>
2021-02-10 17:07:17 +0800

    Added steatrealtime.bat
    Start aria2c with high priority (start.bat) / real time (startrealtime.bat)

--- scripts/start.bat
+++ scripts/start.bat
@@ -5,4 +5,4 @@ for /f "tokens=2 delims==; " %%A in (
 ) do set PID=%%A
 cd /D %~dp0resources\aria2\
 start "" %~dp0resources\aria2\aria2c.exe -D true --conf-path="%~dp0resources\aria2\aria2.conf" --stop-with-process=%PID% 
-wmic process where name='aria2c.exe' call SetPriority "Realtime"
+wmic process where name='aria2c.exe' call SetPriority "high priority"
